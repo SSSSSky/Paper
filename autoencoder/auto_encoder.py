730032722d0b2edf555c1f315cf5e13593cbe0e5
SSSSSky <32289078+SSSSSky@users.noreply.github.com>
2018-03-25 14:08:38 +0800

    Update auto_encoder.py

--- autoencoder/auto_encoder.py
+++ autoencoder/auto_encoder.py
@@ -2,6 +2,8 @@
 AutoEncoder 自编码器
 '''
 import tensorflow as tf
+import os.path
+import pandas as pd
 
 
 class Autoencoder(object):
@@ -65,30 +67,88 @@ class Autoencoder(object):
     def getBiases(self):
         return self.sess.run(self.weights['b1'])
 
+def Concat(i):
+    rootdir = '../feature/features'
+    j = 0
+    q = 0
+
+    for filelist in os.listdir(rootdir):
+        path = os.path.join(rootdir, filelist)
+        for filename in os.listdir(path):
+            if j == i:
+                path2 = os.path.join(path, filename)
+                df1 = pd.read_csv(path2, index_col="DataTime")
+                df1 = df1.unstack(-1).reset_index()
+                df1.columns = ['SecCode', 'DateTime', filelist]
+                if q == 0:
+                    df2 = df1
+                else:
+                    df2 = pd.merge(df1, df2, on=['SecCode', 'DateTime'], how = 'inner')
+                    df2.dropna(axis=0, inplace=True)
+                j = 0
+            else:
+                break
+            j += 1
+        q += 1
+
+    return df2
 
 def main():
-    import pandas as pd
-    df1 = pd.read_csv('../feature/features/return/000016_return_20100104_20100630.csv', index_col='DateTime')
-    df2 = pd.read_csv('../feature/features/atr/000016_atr_20100104_20100630.csv', index_col='DateTime')
-    df3 = pd.read_csv('../feature/features/macd/000016_macd_20100104_20100630.csv', index_col='DateTime')
-    df1 = df1.unstack(-1).reset_index()
-    df1.columns = ['SecCode', 'DateTime', 'return']
+
+    # df1 = pd.read_csv('../feature/features/return/000016_return_20100104_20100630.csv', index_col='DateTime')
+    # df2 = pd.read_csv('../feature/features/atr/000016_atr_20100104_20100630.csv', index_col='DateTime')
+    # df3 = pd.read_csv('../feature/features/macd/000016_macd_20100104_20100630.csv', index_col='DateTime')
+    # df1 = df1.unstack(-1).reset_index()
+    # df1.columns = ['SecCode', 'DateTime', 'return']
     # print(df1.shape[0])
-    df2 = df2.unstack(-1).reset_index()
-    df2.columns = ['SecCode', 'DateTime', 'ATR']
+    # df2 = df2.unstack(-1).reset_index()
+    # df2.columns = ['SecCode', 'DateTime', 'ATR']
     # print(df2.shape[0])
-    df3 = df3.unstack(-1).reset_index()
-    df3.columns = ['SecCode', 'DateTime', 'MACD']
-    join_df = pd.merge(df1, df2, on=['SecCode', 'DateTime'], how='inner')
-    join_df = pd.merge(join_df, df3, on=['SecCode', 'DateTime'], how='inner')
-    print(join_df.shape[0])
-    join_df.dropna(axis=0, inplace=True)
+    # df3 = df3.unstack(-1).reset_index()
+    # df3.columns = ['SecCode', 'DateTime', 'MACD']
+    # join_df = pd.merge(df1, df2, on=['SecCode', 'DateTime'], how='inner')
+    # join_df = pd.merge(join_df, df3, on=['SecCode', 'DateTime'], how='inner')
+    # print(join_df.shape[0])
+    # join_df.dropna(axis=0, inplace=True)
+    # print(join_df.shape[0])
+    # print(join_df.columns)
+    # df1 = pd.read_csv('../feature/features/return/000016_return_20100104_20100630.csv', index_col='DateTime')
+    # df2 = pd.read_csv('../feature/features/atr/000016_atr_20100104_20100630.csv', index_col='DateTime')
+    # df3 = pd.read_csv('../feature/features/macd/000016_macd_20100104_20100630.csv', index_col='DateTime')
+    # df4 = pd.read_csv('../feature/features/logreturn/000016_logreturn_20100104_20100630.csv', index_col='DateTime')
+    # df5 = pd.read_csv('../feature/features/log_vol_chg_rate/000016_log_vol_chg_rate_20100104_20100630.csv',
+    #                   index_col='DateTime')
+    # df6 = pd.read_csv('../feature/features/vol_chg_rate/000016_vol_chg_rate_20100104_20100630.csv',
+    #                   index_col='DateTime')
+    # df7 = pd.read_csv('../feature/features/volume_relative_ratio/000016_volume_relative_ratio_20100104_20100630.csv',
+    #                   index_col='DateTime')
+    # df8 = pd.read_csv('../feature/features/amp/000016_amp_20100104_20100630.csv', index_col='DateTime')
+    # df9 = pd.read_csv('../feature/features/price_efficiency/000016_price_efficiency_20100104_20100630.csv',
+    #                   index_col='DateTime')
+    # df10 = pd.read_csv('../feature/features/pri_ma_ratio/000016_pri_ma_ratio_20100104_20100630.csv',
+    #                    index_col='DateTime')
+    # df11 = pd.read_csv('../feature/features/atr/000016_atr_20100104_20100630.csv', index_col='DateTime')
+    # df12 = pd.read_csv('../feature/features/rsi/000016_rsi_20100104_20100630.csv', index_col='DateTime')
+    # df13 = pd.read_csv('../feature/features/cci/000016_cci_20100104_20100630.csv', index_col='DateTime')
+    # df14 = pd.read_csv('../feature/features/obv/000016_obv_20100104_20100630.csv', index_col='DateTime')
+    # df15 = pd.read_csv('../feature/features/roc/000016_roc_20100104_20100630.csv', index_col='DateTime')
+    # df16 = pd.read_csv('../feature/features/bias/000016_bias_20100104_20100630.csv', index_col='DateTime')
+    # df17 = pd.read_csv('../feature/features/vma/000016_vma_20100104_20100630.csv', index_col='DateTime')
+    # df18 = pd.read_csv('../feature/features/pvt/000016_pvt_20100104_20100630.csv', index_col='DateTime')
+    # df19 = pd.read_csv('../feature/features/mfi/000016_mfi_20100104_20100630.csv', index_col='DateTime')
+    # df20 = pd.read_csv('../feature/features/ma_ma_ratio/000016_ma_ma_ratio_20100104_20100630.csv', index_col='DateTime')
+    # df21 = pd.read_csv('../feature/features/boll/000016_boll_20100104_20100630.csv', index_col='DateTime')
+    # df22 = pd.read_csv('../feature/features/ar/000016_ar_20100104_20100630.csv', index_col='DateTime')
+    # df23 = pd.read_csv('../feature/features/br/000016_br_20100104_20100630.csv', index_col='DateTime')
+
+    join_df = Concat(0)
     print(join_df.shape[0])
     print(join_df.columns)
-    ae = Autoencoder(3, 10, tf.nn.tanh)
-    cost = ae.partial_fit(join_df.loc[:, ['return', 'ATR', 'MACD']])
-    print(cost)
-    print(ae.transform(join_df.loc[:, ['return', 'ATR', 'MACD']]))
+
+    # ae = Autoencoder(3, 10, tf.nn.tanh)
+    # cost = ae.partial_fit(join_df.loc[:, ['return', 'ATR', 'MACD']])
+    # print(cost)
+    # print(ae.transform(join_df.loc[:, ['return', 'ATR', 'MACD']]))
 
 if __name__ == '__main__':
     main()
