5f347452aaee2e1c233e4042ca02491d38c07255
SSSSSky <32289078+SSSSSky@users.noreply.github.com>
2018-03-27 10:07:51 +0800

    Update StackAutoencoder.py

--- StackAutoencoder.py
+++ StackAutoencoder.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 import os.path
 
-N_INPUT = 28*28
+N_INPUT = 153039 * 70
 N_HIDDEN_1 = 10
 N_OUTPUT_1 = N_INPUT
 N_HIDDEN_2 = 15
 N_OUTPUT_2 = N_HIDDEN_1
 N_OUTPUT = 10
-BATCH_SIZE = 16
-EPOCHES = 10
+# BATCH_SIZE = 16
+# EPOCHES = 10
 RHO = .1
 BETA = tf.constant(3.0)
 LAMBDA = tf.constant(.0001)
@@ -85,7 +85,6 @@ model_one_cost_reg = tf.multiply(LAMBDA, tf.add(tf.nn.l2_loss(model_one_weights[
 # cost function
 model_one_cost = tf.add(tf.add(model_one_cost_J, model_one_cost_reg), model_one_cost_sparse)
 train_op_1 = tf.train.AdamOptimizer().minimize(model_one_cost)
-print('model_one_cost:' + model_one_cost)
 # =======================================================================================
 
 # model two
@@ -100,14 +99,12 @@ model_two_cost_reg = tf.multiply(LAMBDA, tf.add(tf.nn.l2_loss(model_two_weights[
 # cost function
 model_two_cost = tf.add(tf.add(model_two_cost_J, model_two_cost_reg), model_two_cost_sparse)
 train_op_2 = tf.train.AdamOptimizer().minimize(model_two_cost)
-print('model_two_cost:' + model_two_cost)
 # =======================================================================================
 
 # final model
 model_out = model(model_one_X)
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(model_out, Y))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = model_out, logits = Y))
 train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
-print('cost:' + cost)
 predict_op = tf.argmax(model_out, 1)
 # =======================================================================================
 
@@ -132,21 +129,34 @@ with tf.Session() as sess:
 
     join_df = get_data()
 
-    for i in range(EPOCHES):
-        for start, end in zip(range(0, len(join_df), BATCH_SIZE), range(BATCH_SIZE, len(join_df), BATCH_SIZE)):
-            input_ = join_df[start:end]
-            sess.run(train_op_1, feed_dict = {model_one_X: input_})
+    cost, opt = sess.run(train_op_1, feed_dict={model_one_X: join_df})
+    print('cost:')
+    print(cost)
     print('finish model one ...')
 
-    for i in range(EPOCHES):
-        for start, end in zip(range(0, len(join_df), BATCH_SIZE), range(BATCH_SIZE, len(join_df), BATCH_SIZE)):
-            input_ = join_df[start:end]
-            input_ = sess.run(tf.sigmoid(tf.add(tf.matmul(input_, model_one_weights["hidden"]), model_one_bias["hidden"])))
-            sess.run(train_op_2, feed_dict = {model_two_X: input_})
+    input_ = sess.run(tf.sigmoid(tf.add(tf.matmul(join_df, model_one_weights["hidden"]), model_one_bias["hidden"])))
+    cost, opt = sess.run(train_op_2, feed_dict = {model_two_X: input_})
+    print('cost:')
+    print(cost)
     print('finish model two ...')
 
-    for i in range(EPOCHES):
-        for start, end in zip(range(0, len(join_df), BATCH_SIZE), range(BATCH_SIZE, len(join_df), BATCH_SIZE)):
-            input_ = join_df[start:end]
-            sess.run(train_op, feed_dict = {model_one_X: input_, Y: join_df[start:end]})
-    print('finish model ...')
+    # for i in range(EPOCHES):
+    #     for start, end in zip(range(0, len(join_df), BATCH_SIZE), range(BATCH_SIZE, len(join_df), BATCH_SIZE)):
+    #         input_ = join_df[start:end]
+    #         cost, opt = sess.run(train_op_1, feed_dict = {model_one_X: input_})
+    #         print('cost:')
+    #         print(cost)
+    # print('finish model one ...')
+    #
+    # for i in range(EPOCHES):
+    #     for start, end in zip(range(0, len(join_df), BATCH_SIZE), range(BATCH_SIZE, len(join_df), BATCH_SIZE)):
+    #         input_ = join_df[start:end]
+    #         input_ = sess.run(tf.sigmoid(tf.add(tf.matmul(input_, model_one_weights["hidden"]), model_one_bias["hidden"])))
+    #         sess.run(train_op_2, feed_dict = {model_two_X: input_})
+    # print('finish model two ...')
+    #
+    # for i in range(EPOCHES):
+    #     for start, end in zip(range(0, len(join_df), BATCH_SIZE), range(BATCH_SIZE, len(join_df), BATCH_SIZE)):
+    #         input_ = join_df[start:end]
+    #         sess.run(train_op, feed_dict = {model_one_X: input_, Y: join_df[start:end]})
+    # print('finish model ...')
