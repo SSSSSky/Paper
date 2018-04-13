8e12afdb9475a91304016f954d2494280b5b1f28
SSSSSky <32289078+SSSSSky@users.noreply.github.com>
2018-04-13 14:27:54 +0800

    Update join_features.py

--- join_features.py
+++ join_features.py
@@ -1,14 +1,6 @@
 import os
 import pandas as pd
 
-def sperate_time(df):
-    train_df_index = df[(df.DataTime.split(' ')[0] == '20150101')].index.tolist()
-    valid_df_index = df[(df.DataTime.split(' ')[0] == '20170101')].index.tolist()
-    train_df = df[0:train_df_index]
-    valid_df = df[train_df_index:valid_df_index]
-    test_df = df[(df.DataTime.split(' ')[0] >= '20170101')]
-    return train_df, valid_df, test_df
-
 def main():
     path = '../feature/join_feature'
     files = os.listdir(path)
@@ -23,12 +15,11 @@ def main():
             pd.concat([join_df, df1])
             # join_df 就是全部的特征数据
 
-    technical_df = join_df.loc[:, ['SecCode', 'DateTime', 'amp', 'ar',
-                                   'atr', 'bias', 'boll', 'br', 'cci', 'log_vol_chg_rate', 'logreturn',
-                                   'ma_ma_ratio', 'macd', 'mfi', 'obv', 'pri_ma_ratio', 'price_efficiency',
-                                   'pvt', 'return', 'roc', 'rsi', 'vma', 'vol_chg_rate',
-                                   'volume_relative_ratio']]
+    # 取10个技术指标
+    technical_df = join_df.loc[:, ['SecCode', 'DateTime', 'bias', 'boll', 'cci',
+                                   'ma_ma_ratio', 'price_efficiency', 'logreturn', 'rsi', 'mfi', 'ar', 'macd']]
 
+    # 取45个alpha指标
     alpha_df = join_df.loc[:, ['SecCode', 'DateTime', 'alpha001', 'alpha002', 'alpha003', 'alpha004',
                                'alpha006', 'alpha007', 'alpha008', 'alpha009', 'alpha010', 'alpha012',
                                'alpha013', 'alpha014', 'alpha015', 'alpha016', 'alpha017', 'alpha018',
@@ -38,22 +29,12 @@ def main():
                                'alpha043', 'alpha044', 'alpha045', 'alpha046', 'alpha049', 'alpha051',
                                'alpha052', 'alpha053', 'alpha054', 'alpha055', 'alpha060']]
 
-    technical_train_df, technical_valid_df, technical_test_df = sperate_time(technical_df)
-    alpha_train_df, alpha_valid_df, alpha_test_df = sperate_time(alpha_df)
+    print(technical_df.shape)
+    print(alpha_df.shape)
 
-    print(technical_train_df.iloc[0])
-    print(technical_train_df.iloc[-1])
-    print(technical_valid_df.iloc[0])
-    print(technical_valid_df.iloc[-1])
-    print(technical_test_df.iloc[0])
-    print(technical_test_df.iloc[-1])
+    pd.DataFrame(data=technical_df, index=technical_df.index, columns=technical_df.columns).to_csv('../feature/technical_all.csv')
+    pd.DataFrame(data=alpha_df, index=alpha_df.index, columns=alpha_df.columns).to_csv('../feature/alpha_all.csv')
 
-    print(alpha_train_df.iloc[0])
-    print(alpha_train_df.iloc[-1])
-    print(alpha_valid_df.iloc[0])
-    print(alpha_train_df.iloc[-1])
-    print(alpha_test_df.iloc[0])
-    print(alpha_test_df.iloc[-1])
 
 if __name__ == '__main__':
     main()
