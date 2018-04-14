1767bebb65de0430cc8ee5885d10b84caf32f6a9
SSSSSky <32289078+SSSSSky@users.noreply.github.com>
2018-04-14 11:22:41 +0800

    Update join_features.py

--- join_features.py
+++ join_features.py
@@ -3,47 +3,56 @@ import pandas as pd
 import os
 import pandas as pd
 
+def seperateData(path):
+    df1 = pd.read_csv(path)
+    print(df1.head())
+
 def main():
-    path = '../feature/join_feature'
-    files = os.listdir(path)
-    files.sort()
-    q = 0
-    for file in files:
-        q += 1
-        file_path = os.path.join(path, file)
-        df1 = pd.read_csv(file_path)
-        if q == 1:
-            join_df = df1
-        else:
-            join_df = pd.concat([join_df, df1])
-            print(join_df.shape)
-            # join_df 就是全部的特征数据
-    join_df = join_df.sort_values(by='DateTime')
-    # 取10个技术指标
-    technical_df = join_df.loc[:, ['SecCode', 'DateTime', 'bias', 'boll', 'cci',
-                                   'ma_ma_ratio', 'price_efficiency', 'logreturn', 'rsi', 'mfi', 'ar', 'macd']]
-
-    # 取45个alpha指标
-    alpha_df = join_df.loc[:, ['SecCode', 'DateTime', 'alpha001', 'alpha002', 'alpha003', 'alpha004',
-                               'alpha006', 'alpha007', 'alpha008', 'alpha009', 'alpha010', 'alpha012',
-                               'alpha013', 'alpha014', 'alpha015', 'alpha016', 'alpha017', 'alpha018',
-                               'alpha019', 'alpha020', 'alpha021', 'alpha022', 'alpha023', 'alpha024',
-                               'alpha026', 'alpha028', 'alpha029', 'alpha030', 'alpha031', 'alpha033',
-                               'alpha034', 'alpha035', 'alpha037', 'alpha038', 'alpha039', 'alpha040',
-                               'alpha043', 'alpha044', 'alpha045', 'alpha046', 'alpha049', 'alpha051',
-                               'alpha052', 'alpha053', 'alpha054', 'alpha055', 'alpha060']]
-
-    print(technical_df.shape)
-    print(alpha_df.shape)
-    print(technical_df.head())
-    print(alpha_df.head())
-
-    pd.DataFrame(data=technical_df, index=technical_df.index, columns=technical_df.columns).to_csv('../feature/technical_all.csv')
-    pd.DataFrame(data=alpha_df, index=alpha_df.index, columns=alpha_df.columns).to_csv('../feature/alpha_all.csv')
+    # path = '../feature/join_feature'
+    # files = os.listdir(path)
+    # files.sort()
+    # q = 0
+    # for file in files:
+    #     q += 1
+    #     file_path = os.path.join(path, file)
+    #     df1 = pd.read_csv(file_path)
+    #     if q == 1:
+    #         join_df = df1
+    #     else:
+    #         join_df = pd.concat([join_df, df1])
+    #         print(join_df.shape)
+    #         # join_df 就是全部的特征数据
+    # join_df = join_df.sort_values(by='DateTime')
+    # # 取10个技术指标
+    # technical_df = join_df.loc[:, ['SecCode', 'DateTime', 'bias', 'boll', 'cci',
+    #                                'ma_ma_ratio', 'price_efficiency', 'logreturn', 'rsi', 'mfi', 'ar', 'macd']]
+    #
+    # # 取45个alpha指标
+    # alpha_df = join_df.loc[:, ['SecCode', 'DateTime', 'alpha001', 'alpha002', 'alpha003', 'alpha004',
+    #                            'alpha006', 'alpha007', 'alpha008', 'alpha009', 'alpha010', 'alpha012',
+    #                            'alpha013', 'alpha014', 'alpha015', 'alpha016', 'alpha017', 'alpha018',
+    #                            'alpha019', 'alpha020', 'alpha021', 'alpha022', 'alpha023', 'alpha024',
+    #                            'alpha026', 'alpha028', 'alpha029', 'alpha030', 'alpha031', 'alpha033',
+    #                            'alpha034', 'alpha035', 'alpha037', 'alpha038', 'alpha039', 'alpha040',
+    #                            'alpha043', 'alpha044', 'alpha045', 'alpha046', 'alpha049', 'alpha051',
+    #                            'alpha052', 'alpha053', 'alpha054', 'alpha055', 'alpha060']]
+    #
+    # print(technical_df.shape)
+    # print(alpha_df.shape)
+    # print(technical_df.head())
+    # print(alpha_df.head())
+    #
+    # pd.DataFrame(data=technical_df, index=technical_df.index, columns=technical_df.columns).to_csv('../feature/technical_all.csv')
+    # pd.DataFrame(data=alpha_df, index=alpha_df.index, columns=alpha_df.columns).to_csv('../feature/alpha_all.csv')
 
     #temp =df1[df1.DateTime == '20150105 0931']
-    #print(temp)   
-    #print(df1[df1.DateTime == '20141231 1500']) 
+    #print(temp)
+    #print(df1[df1.DateTime == '20141231 1500'])
+
+    technical_path = '../feature/technical_all.csv'
+    alpha_path = '../feature/alpha_all.csv'
+    seperateData(technical_path)
+    seperateData(alpha_path)
 
 
 
